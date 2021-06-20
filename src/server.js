66dd72f51aaa1ee8d2c19ee19546c771aba30e14
Guilherme de Almeida Pinto <Guilherme.Pinto@ibm.com>
2021-06-20 13:19:51 -0300

    fix to consider env PORT

--- src/server.js
+++ src/server.js
@@ -16,6 +16,6 @@ mongoose.connect(url, connectionParams)
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log("Server listening at port: 3333")
 });
